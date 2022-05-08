e0796cbe9ad1b22cd0b8b1785a2c1ba87b8ce09e
yysaki <mail@yysaki.com>
2022-05-08 20:10:43 +0900

    appを除く*.rbをlib/に移動

--- spec/clients/twitter_spec.rb
+++ spec/clients/twitter_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative '../../clients/twitter'
+require_relative '../../lib/clients/twitter'
 
 RSpec.describe Client::Twitter do
   describe '#list'
