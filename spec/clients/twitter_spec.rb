b08bd3b98ba4c4cf6c8dd6d6081bab21373c5934
yysaki <mail@yysaki.com>
2022-05-09 23:35:07 +0900

    Clients::Twitterのspecを追加

--- spec/clients/twitter_spec.rb
+++ spec/clients/twitter_spec.rb
@@ -3,6 +3,51 @@
 require 'spec_helper'
 require_relative '../../lib/clients/twitter'
 
-RSpec.describe Client::Twitter do
-  describe '#list'
+RSpec.describe Clients::Twitter do
+  describe '.list' do
+    subject { described_class.list(count: 20) }
+
+    before do
+      client = double('client')
+      allow(Twitter::REST::Client).to receive(:new).and_return(client)
+      allow(client).to receive(:user_timeline).and_return([tweet])
+    end
+
+    context 'when target tweet does not exist in timeline' do
+      let(:tweet) { Twitter::Tweet.new(id: 12_345_678_902, text: 'つぶやき') }
+
+      it 'returns empty array' do
+        expect(subject).to be_empty
+      end
+    end
+
+    context 'when target tweet exists in timeline' do
+      let(:id) { 12_345_678_901 }
+      let(:created_at) { 'Mon May 9 12:34:56 +0000 2022' }
+      let(:photo_url) {  'https://example.com/media/FSNMwAtUUAAju-x?format=jpg&name=large' }
+      let(:tweet) do
+        media = { id: 1, type: 'photo', media_url_https: photo_url }
+        Twitter::Tweet.new(id: id, text: '#RingFitAdventure', created_at: created_at, entities: { media: [media] })
+      end
+      let(:expected) do
+        {
+          status_id: id,
+          tweeted_at: Time.parse(created_at).utc.iso8601.to_s,
+          photo_urls: [photo_url]
+        }
+      end
+
+      it 'returns one element in array' do
+        expect(subject).to be_one
+      end
+
+      it 'returns Status instance' do
+        expect(subject.first).to be_a described_class::Status
+      end
+
+      it 'returns expeted value' do
+        expect(subject.first.to_h).to eq expected
+      end
+    end
+  end
 end
