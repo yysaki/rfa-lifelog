a902d852c98a1a8515e8746bc441da9be3f6ad8b
yysaki <mail@yysaki.com>
2022-05-21 00:43:13 +0900

    Clients::S3::Clientを抽出

--- lib/clients/s3.rb
+++ lib/clients/s3.rb
@@ -18,30 +18,45 @@ module Clients
     end
 
     def create(file_name:, body:)
-      directory = s3.directories.new(key: bucket_name)
-      directory.files.create(key: "#{PREFIX}#{file_name}", body: body)
+      client.create_file(file_name: file_name, body: body)
     end
 
     def list
-      directory = s3.directories.get(bucket_name, prefix: PREFIX)
-      directory.files
-               .map { |file| file.key.gsub(PREFIX, '') }
-               .select { |file_name| file_name.include? '.csv' }
+      client.files
+            .map { |file| file.key.gsub(PREFIX, '') }
+            .select { |file_name| file_name.include? '.csv' }
     end
 
     private
 
-    def s3
-      @s3 ||= Fog::Storage.new(
-        provider: 'AWS',
-        region: 'ap-northeast-1',
-        aws_access_key_id: Settings.clients.s3.aws_access_key_id,
-        aws_secret_access_key: Settings.clients.s3.aws_secret_access_key
-      )
+    def client
+      @client ||= Client.new
     end
 
-    def bucket_name
-      @bucket_name ||= Settings.clients.s3.aws_bucket_name
+    class Client
+      def create_file(file_name:, body:)
+        directory = s3.directories.new(key: bucket_name)
+        directory.files.create(key: "#{PREFIX}#{file_name}", body: body)
+      end
+
+      def files
+        s3.directories.get(bucket_name, prefix: PREFIX).files
+      end
+
+      private
+
+      def s3
+        @s3 ||= Fog::Storage.new(
+          provider: 'AWS',
+          region: 'ap-northeast-1',
+          aws_access_key_id: Settings.clients.s3.aws_access_key_id,
+          aws_secret_access_key: Settings.clients.s3.aws_secret_access_key
+        )
+      end
+
+      def bucket_name
+        Settings.clients.s3.aws_bucket_name
+      end
     end
   end
 end
